35095063b30a51c892aaee5b789dcf095f815271
mixinspace <islammadkaur@gmail.com>
2020-11-09 10:04:52 -0600

    commit tree efa867d390e68bb2155efda17084d6d8b5e0945c

--- solution.rb
+++ solution.rb
@@ -1,7 +1,31 @@
-class Deck
 
+    class Deck
+        attr_reader :cards
+
+        def initialize
+          @cards = []
+          suits = ["spades", "diamonds", "hearts", "clubs"]
+          ranks = ["A","2","3","4","5","6","7","8","9","10","J","Q","K"]
+          suits.each do |suit|
+            ranks.each do |rank|
+              card = Card.new(rank, suit)
+              @cards << card
+            end
+          end
+        end
+
+        def choose_card
+            @cards.delete_at(rand(@cards.count))
+        end
 end
 
 class Card
+
+    attr_reader :rank, :suit
+
+    def initialize(suit, rank)
+        @rank = rank
+        @suit = suit
+    end
 
 end
